0e95e01516358237a7600562074c34945dab0746
DenisdS <denis_santos1989@hotmail.com>
2020-12-17 00:39:07 -0300

    feat(get-your-job):create media highlight

--- src/common/media-highlight/media-highlight.tsx
+++ src/common/media-highlight/media-highlight.tsx
@@ -3,8 +3,22 @@ import React, { ReactElement } from 'react'
 const MediaHighlightComponent = (): ReactElement => {
 
   return (
-    <div>
-    </div>
+    <>
+      <article>
+        <h2>PALAVRA DO CEO</h2>
+        <h3>Carlos Curioni</h3>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisci elit,
+          sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+          nisi ut aliquid ex ea commodi consequatur.
+          Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+      </article>
+      <h2>
+      </h2>
+    </>
   )
 }
 
