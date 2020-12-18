99c0113e4c7b664e426f38aad97a7c2c6e101ce0
DenisdS <denis_santos1989@hotmail.com>
2020-12-17 22:28:10 -0300

    test(get-your-job):update content test's media highlight

--- src/common/media-highlight/media-highlight.test.tsx
+++ src/common/media-highlight/media-highlight.test.tsx
@@ -20,4 +20,40 @@ describe('Header Component', () => {
       unmount()
     })
   })
+
+  describe('Render article', () => {
+    it('should return a article tag', () => {
+      const [wrapper, unmount] = createComponent({})
+
+      expect(wrapper.find('article')).toBeTruthy()
+      unmount()
+    })
+
+    describe('Render image', () => {
+      it('should this is an image in article', () => {
+        const [wrapper, unmount] = createComponent({})
+
+        expect(wrapper.find('article img')).toBeTruthy()
+        unmount()
+      })
+
+      it('should this is alt atribute on image tag', () =>
+        const [wrapper, unmount] = createComponent({})
+
+        expect(wrapper.find('article img').prop('alt')).toBeTruthy()
+        unmount()
+      })
+    })
+
+    describe('Render titles', () => {
+      it('should return content with titles and p tags', () => {
+        const [wrapper, unmount] = createComponent({})
+
+        expect(wrapper.find('article h2')).toBeTruthy()
+        expect(wrapper.find('article h3')).toBeTruthy()
+        expect(wrapper.find('article p')).toBeTruthy()
+        unmount()
+      })
+    })
+  })
 })
