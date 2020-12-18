6d2af3fabba66736a69c7ac413b36182c83b525c
DenisdS <denis_santos1989@hotmail.com>
2020-12-17 22:03:36 -0300

    feat(get-your-job):add info to section about's

--- src/common/media-highlight/media-highlight.tsx
+++ src/common/media-highlight/media-highlight.tsx
@@ -1,25 +1,31 @@
-import React, { ReactElement } from 'react'
+import React, {
+  PureComponent,
+  ReactElement
+} from 'react'
 
-const MediaHighlightComponent = (): ReactElement => {
+export default class MediaHighlightComponent extends PureComponent {
+  imgThumbTalkCEO = `${process.env.PUBLIC_URL}/images/placeholder-video.png`
 
-  return (
-    <>
-      <article>
-        <h2>PALAVRA DO CEO</h2>
-        <h3>Carlos Curioni</h3>
+  render = (): ReactElement => {
+    return (
+      <>
+        <article>
+          <img
+            src={this.imgThumbTalkCEO}
+            alt="talk com o CEO"
+          />
+          <h2>PALAVRA DO CEO</h2>
+          <h3>Carlos Curioni</h3>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisci elit,
-          sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
-          nisi ut aliquid ex ea commodi consequatur.
-          Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        </p>
-      </article>
-      <h2>
-      </h2>
-    </>
-  )
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisci elit,
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+            nisi ut aliquid ex ea commodi consequatur.
+            Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          </p>
+        </article>
+      </>
+    )
+  }
 }
-
-export default MediaHighlightComponent
