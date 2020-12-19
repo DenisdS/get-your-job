d78f0bf8d1298171399066ff6c8cc9b6a630d5db
DenisdS <denis_santos1989@hotmail.com>
2020-12-19 16:45:38 -0300

    feat(get-your-job): add tools accessibility

--- src/home/Index.tsx
+++ src/home/Index.tsx
@@ -10,12 +10,57 @@ import Jobs from '../jobs/index'
 import OurValues from '../our-values/index'
 
 import '../style/basic.scss'
-export default class Home extends PureComponent {
+
+type Props = {}
+
+type State = {
+  contrast: boolean
+}
+export default class Home extends PureComponent<Props, State> {
+
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      contrast: localStorage.getItem('contrast') === 'true' ? true : false
+    }
+  }
+
+  componentDidMount = async (): Promise<void> => {
+    try {
+      await this.checkAccessibility()
+    } catch (error) {
+      window.console.error(`Error: checkAccessibility \n ${error}`)
+    }
+  }
+
+  checkAccessibility = (): void => {
+    const { contrast } = this.state
+
+    contrast ? document.body.classList.add('home-contrast') : document.body.classList.remove('home-contrast')
+  }
+
+  accessibility = (): void => {
+    const { contrast } = this.state
+
+    if (!contrast) {
+      document.body.classList.add('home-contrast')
+      this.setState({ contrast: true  })
+      localStorage.setItem('contrast', JSON.stringify(true))
+    } else {
+      document.body.classList.remove('home-contrast')
+      this.setState({ contrast: false  })
+      localStorage.setItem('contrast', JSON.stringify(false))
+    }
+  }
 
   render = (): ReactElement => {
 
     return (
       <div className="home" >
+        <p className="toolsAccessibility" onClick={this.checkAccessibility}>
+          Acessibilidade - Contraste
+        </p>
         <HeaderComponent />
         <About />
         <OurValues />
--- src/home/home.test.tsx
+++ src/home/home.test.tsx
@@ -65,4 +65,35 @@ describe('Home Component', () => {
     expect(wrapper.find('.home').exists()).toBeTruthy()
     unmount()
   })
+
+  describe('Functions', () => {
+    it('Should call checkAccessibility on componentDidMount', async () => {
+      const mock = jest.fn()
+      const [wrapper, unmount] = createComponent()
+
+      wrapper.instance().checkAccessibility = mock
+      await wrapper.instance().componentDidMount()
+
+      expect(wrapper.instance().checkAccessibility).toHaveBeenCalled()
+      unmount()
+    })
+
+    it('Should contrast when call the accessibility and constrast is false', async () => {
+      const [wrapper, unmount] = createComponent()
+
+      wrapper.setState({ contrast: false })
+      wrapper.instance().accessibility()
+      expect(wrapper.state('contrast')).toBeTruthy()
+      unmount()
+    })
+
+    it('Should remove contrast when call the accessibility and constrast is true', async () => {
+      const [wrapper, unmount] = createComponent()
+
+      wrapper.setState({ contrast: true })
+      wrapper.instance().accessibility()
+      expect(wrapper.state('contrast')).toBeFalsy()
+      unmount()
+    })
+  })
 })
